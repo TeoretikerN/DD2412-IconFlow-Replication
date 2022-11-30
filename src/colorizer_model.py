af1efd03208612bca4980f32ccf21a2561e663f9
Teo Jansson Minne <96913547+TeoretikerN@users.noreply.github.com>
2022-11-30 16:47:31 +0100

    Update colorizer_model.py

--- src/colorizer_model.py
+++ src/colorizer_model.py
@@ -2,12 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import pytorch_lightning as pl
 from torchvision.models import resnet18, resnet50
 from unet import Unet
 from normconv import NormConv
 
 
-class Colorizer(nn.Module):
+class Colorizer(pl.LightningModule):
     def __init__(self,
                  contour_dim=1,
                  image_dim=3,
@@ -16,7 +17,8 @@ class Colorizer(nn.Module):
                  style_dim=48,
                  decoder_dim=32,
                  decoder_depth=4,
-                 toy_model=False):
+                 toy_model=False,
+                 lr=1e-4):
         super(Colorizer, self).__init__()
         self.contour_dim = contour_dim
         self.image_dim = image_dim
@@ -26,8 +28,9 @@ class Colorizer(nn.Module):
         self.decoder_dim = decoder_dim
         self.decoder_depth = decoder_depth
         self.resnet = resnet18() if toy_model else resnet50()
+        self.lr = lr
         
-        self.image_encoder = nn.Sequential(
+        self.contour_encoder = nn.Sequential(
             Unet(self.contour_dim,
                  self.embedding_dim,
                  depth=5,
@@ -35,7 +38,7 @@ class Colorizer(nn.Module):
             nn.Tanh()
         )
         
-        self.image_extractor = nn.Sequential(
+        self.contour_extractor = nn.Sequential(
             NormConv(self.norm_dim),
             Unet(self.norm_dim,
                  self.contour_dim,
@@ -59,11 +62,13 @@ class Colorizer(nn.Module):
         decoder_list.append(nn.Linear(dim_in, self.decoder_dim))
         decoder_list.append(nn.LayerNorm(self.decoder_dim))
         decoder_list.append(nn.ReLU())
+        # TODO: Force in range (-.5, .5), divide by 2?
         return decoder_list
     
     def forward(self, contour, image):
-        enc_c = self.image_encoder(contour)
+        enc_c = self.contour_encoder(contour)
         enc_s = self.style_encoder(image)
+        enc_s_cc = enc_s.roll(1, 0).contiguous()
         
         c0, c1 = enc_c.shape
         s0, s1, s2, s3 = enc_s.shape
@@ -72,7 +77,51 @@ class Colorizer(nn.Module):
         z = torch.transpose(z,1,3)
         z = torch.transpose(z,1,2)
         
+        z_cc = torch.cat([enc_c, enc_s_cc], 1)
+        z_cc = torch.transpose(z_cc,1,3)
+        z_cc = torch.transpose(z_cc,1,2)
+        
         out = self.decoder(z)
-        out = torch.transpose(z,1,3)
-        out = torch.transpose(z,2,3)
-        return out
+        out = torch.transpose(out,1,3)
+        out = torch.transpose(out,2,3)
+        
+        out_cc = self.decoder(z_cc)
+        out_cc = torch.transpose(out_cc,1,3)
+        out_cc = torch.transpose(out_cc,2,3)
+        return out, out_cc
+    
+    def countour_extract(self, image):
+        return self.contour_extractor(image)
+    
+    def training_step(self, batch, batch_idx):
+        loss = 0
+        image, contour = batch
+        colorized, colorized_cc = self(contour, image)
+        extracted_contour = self.contour_extractor(image)
+        
+        # Get contour for consistency criterion (not pushing gradients)
+        self.contour_extractor.requires_grad_(False)
+        extracted_contour_cc = self.contour_extractor(colorized_cc)
+        self.contour_extractor.requires_grad_(True)
+        
+        # MSE
+        rec_loss = F.mse_loss(colorized, image)
+        
+        # Contour (TODO: Check shape, might need stack + mean)
+        contour_loss = F.mse_loss(extracted_contour, contour)
+        
+        # Consistency criterion (TODO: Check shape, might need stack + mean)
+        consistency_loss = F.mse_loss(extracted_contour_cc, contour)
+        
+        loss = rec_loss + contour_loss + consistency_loss
+        
+        # Logging to tensorboard
+        self.log('Rec_Loss', rec_loss)
+        self.log('Contour_Loss', contour_loss)
+        self.log('Consistency_Loss', consistency_loss)
+        self.log('Loss', loss)
+        
+        return loss
+    
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
