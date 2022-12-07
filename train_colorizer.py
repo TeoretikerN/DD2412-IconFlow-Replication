6c3dd46c45911faa1b545abe7ae9c7f589791d8f
zeknife <zeknife@gmail.com>
2022-12-07 20:27:08 +0100

    Made training based on epochs

--- train_colorizer.py
+++ train_colorizer.py
@@ -15,7 +15,6 @@ batch_size = 32
 image_size = 64
 train_ratio = 0.9
 
-
 if __name__ == "__main__":
     # Dataset initialization copied from iconflow __main__.py
     # https://github.com/djosix/IconFlow
@@ -23,8 +22,7 @@ if __name__ == "__main__":
     train_set = IconContourDataset(data_dir, image_size, split=(0, train_ratio))
     test_set = IconContourDataset(data_dir, image_size, split=(train_ratio, 1))
     net_train_set = IconContourDataset(
-        data_dir,
-        image_size,
+        data_dir, image_size,
         random_crop=True,
         random_transpose=True,
         random_color=True,
@@ -40,25 +38,15 @@ if __name__ == "__main__":
     )
 
     for image, contour in train_loader:
-        print('image type:', type(image), 'image shape:', image.shape)
-        print('contour type:', type(contour), 'contour shape:', contour.shape)
+        print(type(image))
+        print(type(contour))
+        print(image.shape)
+        print(contour.shape)
         break
 
-    print("Num batches:", len(train_loader.dataset))
-
-    # Define model
-    # encoder = nn.Sequential(nn.Conv2d(1, 10, 3, stride=2, padding=1), nn.Conv2d(10, 20, 3, stride=2, padding=1))
-    # decoder = nn.Sequential(nn.ConvTranspose2d(20, 10, 3, stride=2, padding=1, output_padding=1), nn.ConvTranspose2d(10, 3, 3, stride=2, padding=1, output_padding=1))
-    # encoder = nn.Sequential(nn.Linear(image_size**2, 64), nn.ReLU(), nn.Linear(64, 16))
-    # decoder = nn.Sequential(nn.Linear(16, 64), nn.ReLU(), nn.Linear(64, 3*image_size**2))
-
-    # summary(encoder, (1, image_size, image_size))
-    # summary(decoder, (20, image_size//4, image_size//4))
-    # model = TestModel(encoder, decoder)
-
     model = Colorizer()
-    summary(model.contour_encoder, input_size=(batch_size, 1, image_size, image_size))
+    summary(model.contour_encoder, input_size=(batch_size,1,image_size,image_size))
 
-    trainer = pl.Trainer(max_epochs=10, accelerator="gpu")
+    trainer = pl.Trainer(max_epochs=100, accelerator="gpu")
     trainer.fit(model=model, train_dataloaders=train_loader)
 
